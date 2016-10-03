daa74d905a7000a477d336e084993cfef592172e
simonsayswhaat <simonmill@gmail.com>
2016-10-03 14:41:12 +0200

    Updated Css Notes and started jQuery.js notes

---
+++ jQuery.js
@@ -0,0 +1,22 @@
+jQuery Basics Overview
+----------------------
+
+Prerequisite Treehouse Courses
+
+JavaScript Basics // https://teamtreehouse.com/library/javascript-basics Done
+JavaScript Loops, Arrays and Objects // https://teamtreehouse.com/library/javascript-loops-arrays-and-objects Done
+
+Further Reading
+
+Why jQuery is the Most Popular JavaScript Library // http://blog.teamtreehouse.com/jquery-popular-javascript-library
+What is Unobtrusive JavaScript and Why it’s Important? // http://blog.teamtreehouse.com/unobtrusive-javascript-important
+
+------------------
+  What is jQuery
+------------------
+Further Reading
+Beginner’s Guide to jQuery //http://blog.teamtreehouse.com/beginners-guide-to-jquery
+
+Documentation
+.hide() //http://api.jquery.com/hide/
+.show() //http://api.jquery.com/show/
